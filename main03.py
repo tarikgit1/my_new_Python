febf15cab2c98965be2f4be0da9394c1833c82ef
tarik.git1 <tarik.git@gmail.com>
2025-05-10 16:37:29 +0100

    initial commit

---
+++ main03.py
@@ -0,0 +1,31 @@
+
+import git 
+
+repo_url = "https://github.com/tarikgit1/test.git"
+
+clone_dir  = "D:/python development/repo2"
+
+output = git.Repo.clone_from(repo_url,clone_dir)
+
+print(output)
+
+
+#provide the tokem
+
+
+# repo_url = "https://github.com/tarikgit1/repo.git"
+
+# clone_url = "D:/python development/repo"
+
+# # token = "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"
+
+# # g = Github(token)
+
+# # user = g.get_user()
+
+# # repo_name = "my_new_Python"
+# # repo = user.create_repo(repo_name)
+# # print(f"Repository {repo_name} created successfully")
+
+# subprocess.run(["git","clone",repo_url,clone_url])
+
